117fa098a445f6398d131a9f89e2d505c15c5675
danjarosz <jarosz.daniel@protonmail.com>
2021-02-04 19:42:15 +0100

    feat: quiz

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const Quiz = require("../models/quiz");
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("quiz", { title: "Quiz" });
+  const show = !req.session.vote;
+  Quiz.find((err, data) => {
+    let sum = 0;
+    data.forEach((item) => {
+      sum = sum + item.vote;
+    });
+
+    res.render("quiz", { title: "Quiz", data, show, sum });
+  });
+});
+
+router.post("/", (req, res) => {
+  const { quiz } = req.body;
+
+  Quiz.findOne({ _id: quiz }, (err, data) => {
+    data.vote = data.vote + 1;
+    data.save((err) => {
+      req.session.vote = 1;
+      res.redirect("/quiz");
+    });
+  });
 });
 
 module.exports = router;
